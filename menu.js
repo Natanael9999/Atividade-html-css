0b110c6dd1a7bb71b55bd7918a527d4bdaca1036
Natanael9999 <samn14032622@gmail.com>
2024-11-17 17:44:27 -0300

    Css Grid e responsividade
    Atualização gerais

---
+++ menu.js
@@ -0,0 +1,6 @@
+var espandere = document.querySelector('#espandere')
+var menuSide = document.querySelector('.menu')
+
+espandere.addEventListener('click', function(){
+    menuSide.classList.toggle('expandir')
+})
